4bc1e6181e667c187fd9b17d45400f69fd9af116
Here <programaciondairo@gmail.com>
2023-08-05 16:08:04 +0200

    feat: creation factory with migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Estudiante;
+use App\Models\Tarea;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        Estudiante::factory()->times(15)->create();
+        Tarea::factory()->times(8)->create()->each(function($tarea){
+            $tarea->estudiantes()->sync(
+                Estudiante::all()->random(3)
+            );
+        });
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
