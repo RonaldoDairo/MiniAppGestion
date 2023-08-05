3033b5dc0ac24a795b3aa89d1440b5d82d0ecc4b
Here <programaciondairo@gmail.com>
2023-08-05 15:12:52 +0200

    feat: details in models

--- app/Models/Estudiante.php
+++ app/Models/Estudiante.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Estudiante extends Model
 {
     use HasFactory;
+
+    public $table = "estudiantes";
+    protected $fillable = array("*");
+
+    public function tareas(){
+        return $this->belongsToMany(Tarea::class,"estudiante_tarea");
+    }
 }
--- app/Models/Tarea.php
+++ app/Models/Tarea.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Tarea extends Model
 {
     use HasFactory;
+
+    public $table = "tareas";
+    protected $fillable = array("*");
+
+    public function Estudiantes(){
+        return $this->belongsToMany(Estudiante::class,"estudiante_tarea");
+    }
 }
